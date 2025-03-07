f14349d5878d564193c4a25510f9099b99238552
Burak Yildiz <burakkyildiz@icloud.com>
2025-03-08 00:28:15 +0300

    Add ui elements and actions

--- SystemViewController/ViewController.swift
+++ SystemViewController/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Elements
+    @IBOutlet weak var imageView: UIImageView!
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    // MARK: - Functions
+    
+    // MARK: - Actions
 
-
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
